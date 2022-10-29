53d981dd1f42c0b34a12b8f5095d804647947bca
roiaki <roitoitoitoi@yahoo.co.jp>
2022-10-29 14:47:25 +0900

    gitのキャッシュを削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Auth::routes();
 Route::get('/', 'ArticleController@index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
